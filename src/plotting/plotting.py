10f678a4d4cf65aa8500ea9c9bb9ad0c260e16ca
JMGO-coding <gochema00@gmail.com>
2025-02-17 12:33:33 +0100

    Update plotting.py

--- src/plotting/plotting.py
+++ src/plotting/plotting.py
@@ -156,7 +156,7 @@ def plot_arm_statistics(arm_stats: List[dict], algorithms: List[Algorithm], opti
         
     # Ahora llamamos a la función de visualización de histogramas pasándole los datos de entrada 
     n = len(algorithms)
-    data_list = [[arm_stats[j][key]["media"] for key in arm_stats[j].keys()] for j in range(len(arm_stats))]
+    data_list = [[arm_stats[j][key]["reward"] for key in arm_stats[j].keys()] for j in range(len(arm_stats))]
     configs = [{"color": "gray", "bins": len(arm_stats[0].keys()), "title": f"Histograma {get_algorithm_label(alg)}", "highlight_color": "red"} for alg in algorithms]
     highlight_bars = [[num] for num in optimal_arms_list]
     str_choices = [["N"+str(j+1)+": "+str(num_choices_list[i][j]) for j in range(len(num_choices_list[i]))] for i in range(len(num_choices_list))]
