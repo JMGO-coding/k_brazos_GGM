46ac33159a5f7ab9fda3a3c16f84b0723552a682
AnaGM2 <anagilmolina2@gmail.com>
2025-02-19 11:40:12 +0100

    Update softmax.py

--- src/algorithms/softmax.py
+++ src/algorithms/softmax.py
@@ -1,1 +1,43 @@
+"""
+Module: algorithms/softmax.py
+Description: Implementación del algoritmo Softmax para el problema de los k-brazos.
 
+This software is licensed under the GNU General Public License v3.0 (GPL-3.0),
+with the additional restriction that it may not be used for commercial purposes.
+
+For more details about GPL-3.0: https://www.gnu.org/licenses/gpl-3.0.html
+"""
+
+import numpy as np
+
+from algorithms.algorithm import Algorithm
+
+class Softmax(Algorithm):
+
+    def __init__(self, k: int, tau: float):
+        """
+        Inicializa el algoritmo Softmax.
+
+        :param k: Número de brazos.
+        :param tau: Parámetro que controla el equilibrio entre exploración y explotación.
+        :raises ValueError: Si tau no es mayor que 0.
+        """
+        assert tau > 0, "El parámetro tau debe ser mayor que 0."
+
+        super().__init__(k)
+        self.tau = tau
+
+    def select_arm(self) -> int:
+        """
+        Selecciona un brazo basado en la política Softmax.
+        :return: índice del brazo seleccionado.
+        """
+
+        # Calcular las probabilidades de selección para cada brazo
+        exp_values = np.exp(self.values / self.tau)
+        prob = exp_values / np.sum(exp_values)
+
+        # Seleccionar un brazo de acuerdo con la distribución de probabilidad calculada
+        chosen_arm = np.random.choice(self.k, p=prob)
+
+        return chosen_arm
