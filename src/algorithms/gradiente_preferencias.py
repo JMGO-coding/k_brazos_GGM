5fc2e4b2823abf7a4b81ac9757e1df9cb1f9f715
JMGO-coding <gochema00@gmail.com>
2025-02-20 10:56:09 +0100

    Update gradiente_preferencias.py

--- src/algorithms/gradiente_preferencias.py
+++ src/algorithms/gradiente_preferencias.py
@@ -1,1 +1,46 @@
+import numpy as np
+
+from algorithms.algorithm import Algorithm
+
+class GradienteDePreferencias(Algorithm):
+    def __init__(self, k: int, alpha: float = 0.1):
+        """
+        Inicializa el algoritmo de Gradiente de Preferencias para el problema del bandido de k brazos.
+
+        :param k: Número de brazos.
+        :param alpha: Tasa de aprendizaje para actualizar las preferencias.
+        """
+        super().__init__(k)
+        self.alpha = alpha                            # Tasa de aprendizaje
+        self.preferences = np.zeros(k)                # Preferencias iniciales de cada brazo inicializadas a 1 par cada brazo
+        self.action_probabilities = np.ones(k) / k    # Probabilidades iniciales uniformes
+        self.average_reward = 0                       # Promedio de las recompensas recibidas
+        self.time_step = 0                            # Contador de iteraciones
+
+    def select_arm(self) -> int:
+        """
+        Selecciona un brazo basado en la distribución de probabilidad Softmax sobre las preferencias.
+        :return: índice del brazo seleccionado.
+        """
+        exp_preferences = np.exp(self.preferences - np.max(self.preferences))   # Evita desfases numéricos
+        self.action_probabilities = exp_preferences / np.sum(exp_preferences)   # Softmax sobre las preferencias
+        return np.random.choice(self.k, p=self.action_probabilities)            # Selección de acciones (brazos) equiprobable
+
+    def update(self, chosen_arm: int, reward: float):
+        """
+        Actualiza las preferencias del algoritmo basándose en la recompensa obtenida.
+
+        :param chosen_arm: Índice del brazo seleccionado.
+        :param reward: Recompensa recibida.
+        """
+        self.time_step += 1
+        self.average_reward += (reward - self.average_reward) / self.time_step  # Actualización incremental de la recompensa promedio
+        
+        # Actualización de preferencias de cada brazo según el gradiente de preferencias
+        baseline = self.average_reward
+        for arm in range(self.k):
+            if arm == chosen_arm:
+                self.preferences[arm] += self.alpha * (reward - baseline) * (1 - self.action_probabilities[arm])
+            else:
+                self.preferences[arm] -= self.alpha * (reward - baseline) * self.action_probabilities[arm]
 
